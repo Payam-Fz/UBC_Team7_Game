ec5994da91744bc4706a06d9b1a4ddab7b22bb97
Alex <37099150+RA10000BC@users.noreply.github.com>
2021-01-10 14:44:48 -0800

    added the dialogue

---
+++ dialogue Trial 2/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [TextArea(10, 19)] [SerializeField] string dialogueText;
+    [SerializeField] Dialogue[] nextDialogue;
+    
+    public string GetDialogueStory()
+    {
+        return dialogueText;
+    }
+    public Dialogue[] GetNextDialogue()
+    {
+        return nextDialogue; 
+    }
+    
+    
+    
+  
+
+
+
+}

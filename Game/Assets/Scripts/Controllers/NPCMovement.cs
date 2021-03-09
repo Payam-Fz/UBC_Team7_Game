3ee5ce3bceb12532916fce4b47d46fe423d06cf9
payam <payam.4uzandeh@gmail.com>
2021-03-09 12:42:46 -0800

    NPC movement script

---
+++ Game/Assets/Scripts/Controllers/NPCMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float speed = 2f;
+
+    int waypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetposition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetposition, movementThisFrame);
+
+            if (transform.position == targetposition)
+            {
+                waypointIndex++;
+            }
+        }
+
+    }
+}
